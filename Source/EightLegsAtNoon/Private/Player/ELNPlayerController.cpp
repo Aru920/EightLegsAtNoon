65dac26849b4762af4a11a28a21ea13437953525
Aru920 <lhawyhai@gmail.com>
2026-07-08 16:09:17 +0530

    Add pause menu helpers

--- Source/EightLegsAtNoon/Private/Player/ELNPlayerController.cpp
+++ Source/EightLegsAtNoon/Private/Player/ELNPlayerController.cpp
@@ -1,9 +1,17 @@
 #include "Player/ELNPlayerController.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
 void AELNPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	SetGameplayInputMode();
+}
+
+void AELNPlayerController::SetGameplayInputMode()
+{
 	bShowMouseCursor = true;
 	bEnableClickEvents = true;
 	bEnableMouseOverEvents = true;
@@ -13,4 +21,80 @@ void AELNPlayerController::BeginPlay()
 	InputMode.SetHideCursorDuringCapture(false);
 	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	SetInputMode(InputMode);
+}
+
+void AELNPlayerController::SetMenuInputMode()
+{
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeUIOnly InputMode;
+	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	SetInputMode(InputMode);
+}
+
+void AELNPlayerController::PauseGame()
+{
+	if (IsGamePaused())
+	{
+		return;
+	}
+
+	UGameplayStatics::SetGamePaused(this, true);
+	SetMenuInputMode();
+	OnPauseStateChanged(true);
+}
+
+void AELNPlayerController::ResumeGame()
+{
+	if (!IsGamePaused())
+	{
+		return;
+	}
+
+	UGameplayStatics::SetGamePaused(this, false);
+	SetGameplayInputMode();
+	OnPauseStateChanged(false);
+}
+
+void AELNPlayerController::TogglePause()
+{
+	if (IsGamePaused())
+	{
+		ResumeGame();
+	}
+	else
+	{
+		PauseGame();
+	}
+}
+
+void AELNPlayerController::RestartCurrentLevel()
+{
+	UGameplayStatics::SetGamePaused(this, false);
+
+	const FString CurrentLevelName = UGameplayStatics::GetCurrentLevelName(this, true);
+	UGameplayStatics::OpenLevel(this, FName(*CurrentLevelName), false);
+}
+
+void AELNPlayerController::ReturnToMainMenu()
+{
+	UGameplayStatics::SetGamePaused(this, false);
+
+	if (!MainMenuLevelName.IsNone())
+	{
+		UGameplayStatics::OpenLevel(this, MainMenuLevelName, false);
+	}
+}
+
+void AELNPlayerController::QuitGame()
+{
+	UKismetSystemLibrary::QuitGame(this, this, EQuitPreference::Quit, false);
+}
+
+bool AELNPlayerController::IsGamePaused() const
+{
+	return UGameplayStatics::IsGamePaused(this);
 }
--- Source/EightLegsAtNoon/Public/Player/ELNPlayerController.h
+++ Source/EightLegsAtNoon/Public/Player/ELNPlayerController.h
@@ -9,6 +9,40 @@ class EIGHTLEGSATNOON_API AELNPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
+public:
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void SetGameplayInputMode();
+
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void SetMenuInputMode();
+
+	UFUNCTION(BlueprintCallable, Category = "Pause")
+	void PauseGame();
+
+	UFUNCTION(BlueprintCallable, Category = "Pause")
+	void ResumeGame();
+
+	UFUNCTION(BlueprintCallable, Category = "Pause")
+	void TogglePause();
+
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void RestartCurrentLevel();
+
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void ReturnToMainMenu();
+
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void QuitGame();
+
+	UFUNCTION(BlueprintPure, Category = "Pause")
+	bool IsGamePaused() const;
+
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Pause")
+	void OnPauseStateChanged(bool bIsPaused);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Game")
+	FName MainMenuLevelName = "MainMenu";
 };
